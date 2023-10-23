19b12a672f1197a1e770a2121044b193cdd01bc6
Давид Суранов <annasteinway1@gmail.com>
2023-10-23 23:17:06 +1000

    Header dropdown / Product page : Pay Later section

--- src/js/main.js
+++ src/js/main.js
@@ -213,6 +213,7 @@ const header = {
   init: function () {
     this.renderDOM();
     this.bindEvents();
+    this.setDropdowns()
   },
   renderDOM: function () {
     // Login Dropdown
@@ -254,6 +255,100 @@ const header = {
       }
     });
   },
+  setDropdowns: function(...args) {
+    args = Array.from(document.querySelectorAll('.header__sub-link'))
+  
+    if (args.length !== 0 && args) {
+  
+      const dd = document.querySelector('.nav-drop')
+      const cont = [...document.querySelectorAll('.nav-drop__content')]
+      const subLink = [...document.querySelectorAll('.nav-drop_more')]
+      const IS_ACTIVE = 'is-active'
+  
+      let hideDelayTime, showDelayTime
+  
+      if (dd && cont && subLink) {
+        const pos = (el) => {
+          return {
+            top: el.getBoundingClientRect().top + el.offsetHeight,
+            left: el.getBoundingClientRect().left + (el.offsetWidth / 2) - (dd.offsetWidth / 2)
+          }
+        }
+        
+        const fn = {
+          initial: () => {
+            dd.style.top = `${pos(args[0]).top}px`
+            dd.style.left = `${pos(args[0]).left}px`
+          },
+          hide: () => {
+            dd.style.opacity = 0
+            dd.style.display = 'none'
+            args.forEach(el => el.classList.remove(IS_ACTIVE))
+          },
+          show: (el) => {
+            args.forEach(el => el.classList.remove(IS_ACTIVE))
+            el.classList.add(IS_ACTIVE)
+            dd.style.display = 'block'
+            dd.style.top = `${pos(el).top}px`
+            dd.style.left = `${pos(el).left}px`
+            dd.style.opacity = 1
+          },
+          switchContent: (attr) => {
+            cont.forEach(el => el.style.display = 'none')
+            cont.filter(el => el.id == `dd_${attr}`).forEach(el => el.style.display = 'block')
+          }
+        }
+  
+        const { initial: setInitial, hide: hide, show: show, switchContent: switchContent } = fn
+  
+        for (let i = 0; i < subLink.length; i++) {
+          const el = subLink[i],
+            content = el.querySelector('.nav-drop__sub')
+          el.onmouseover = () => { content.style.display = 'block' }
+          el.onmouseleave = () => { content.style.display = 'none' }
+        }
+  
+        dd.onmouseover = (e) => {
+          if (hideDelayTime) window.clearTimeout(hideDelayTime)
+        }
+  
+        dd.onmouseleave = (e) => {
+          window.clearTimeout(showDelayTime)
+          hideDelayTime = window.setTimeout(() => {
+            hide()
+          }, 150);
+        }
+  
+  
+        args.forEach((el) => {
+          el.onmouseover = (e) => {
+            const attr = el.dataset.dropdown
+            if (attr) {
+              window.clearTimeout(hideDelayTime)
+              showDelayTime = window.setTimeout(() => {
+                switchContent(attr)
+                show(el)
+              }, 200);
+            }
+          }
+  
+          el.onmouseleave = (e) => {
+            window.clearTimeout(showDelayTime)
+            hideDelayTime = window.setTimeout(() => {
+              hide()
+            }, 150);
+          }
+        })
+  
+        window.onscroll = () => {
+          window.clearTimeout(showDelayTime)
+          hide(); setInitial()
+        }
+  
+        setInitial()
+      }
+    }
+  }
 };
 /* #endregion */
 
@@ -1422,6 +1517,30 @@ const productPage = new Object({
         });
       }
     },
+    attachPayLaterBoxEvents: function(...args) {
+      args = [...document.querySelectorAll('[data-paylater]')]
+    
+      const intro = document.getElementById('payLaterBoxIntro')
+      const details = document.getElementById('payLaterBoxDetails')
+    
+      if (args && args.length && details) {
+        const toggle = (cond) => {
+          if (cond) {
+            intro.style.display = 'none'; details.style.display = 'block'
+          } else {
+            intro.style.display = 'flex'; details.style.display = 'none'
+          }
+        }
+    
+        args.forEach(el => el.onclick = () => {
+          if (window.getComputedStyle(intro).display == 'none') {
+            toggle(0)
+          } else {
+            toggle(1)
+          }
+        })
+      }
+    }
   },
 });
 /* #endregion */
@@ -1788,4 +1907,4 @@ const initPageObjects = () => {
 document.addEventListener("DOMContentLoaded", function () {
   initPageObjects();
   initTelInput();
-});
+})
