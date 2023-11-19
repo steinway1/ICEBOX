8683a50a66aa65a140630c1042dd407b3a02e75a
Давид Суранов <annasteinway1@gmail.com>
2023-11-20 09:03:35 +1000

    Jura 19 Nov task

--- build/js/partial.js
+++ build/js/partial.js
@@ -2923,6 +2923,129 @@ const bfsModal = new Object({
   }
 })
 
+const quizModal = {
+  currentStep: undefined,
+  isFinished: undefined,
+  pollLength: undefined,
+  maxStep: undefined,
+  isInitialized: undefined,
+  init: function () {
+    this.renderDOM()
+    this.setInitial()
+    this.bindEvents()
+  },
+  renderDOM: function () {
+    this.modal = $('.quiz-modal')
+    this.container = this.modal.find('.quiz-container')
+    this.backdrop = this.modal.find('.quiz-modal__backdrop')
+    this.heightContainer = this.modal.find('.quiz-container-height')
+    this.poll = this.modal.find('.quiz-poll')
+    this.pollArr = [...this.poll]
+    this.answers = this.modal.find('.quiz-answer')
+    this.fullStepText = this.modal.find('.quiz-step-text')
+    this.currentStepText = this.modal.find('.quiz-current-step')
+    this.maxStepText = this.modal.find('.quiz-max-step')
+    this.bar = this.modal.find('.quiz-bar')
+    this.evtGoNext = $('[data-evt="quizGoNext"]')
+    this.evtOpen = $('[data-evt="openQuizModal"]')
+    this.closeBtn = $('[data-evt="closeQuizModal"]')
+  },
+  bindEvents: function () {
+    this.evtGoNext.click(function () {
+      quizModal.goNextStep()
+    })
+    this.closeBtn.click(function () {
+      quizModal.close()
+    })
+    this.evtOpen.click(function() {
+      quizModal.open()
+    })
+  },
+  setInitial: function () {
+    this.currentStep = 1, this.isFinished = false, this.pollLength = this.pollArr.length, this.maxStep = this.pollLength
+    if (this.pollLength !== 0 && this.pollLength !== undefined && this.isInitialized !== true) {
+      this.isInitialized = true
+      quizModal.currentStepText.html(quizModal.currentStep)
+      quizModal.maxStepText.html(quizModal.maxStep)
+      this.modal.find('input[type="radio"]').prop('checked', false); this.evtGoNext.html('Next')
+      // Object.assign(this.heightContainer[0].style, { height: `${this.getObjectScrollHeight(this.pollArr[0])}` })
+      Object.assign(this.bar[0].style, { width: `${(100 / this.pollLength)}%` })
+    } else {
+      return false
+    }
+  },
+  // utils
+  getObjectScrollHeight: function (obj) {
+    if (obj instanceof jQuery) {
+      return `${obj[0].scrollHeight}px`
+    } else {
+      return `${obj.scrollHeight}px`
+    }
+  },
+  noEmptyAnswers: function () {
+    let parent = $(quizModal.pollArr[(quizModal.currentStep - 1)]),
+      checkedRadio = parent.find('input[type="radio"]:checked'),
+      answers = parent.find(quizModal.answers)
+    if (checkedRadio.length == 0) {
+      answers.css({ opacity: '0.3' }); setTimeout(() => { answers.css({ opacity: 1 }) }, 375);
+    } else {
+      return true
+    }
+  },
+  goNextStep() {
+    if (quizModal.currentStep !== quizModal.maxStep) {
+      if (quizModal.noEmptyAnswers()) {
+        Object.assign(quizModal.pollArr[(quizModal.currentStep - 1)].style, { opacity: 0 })
+        Object.assign(quizModal.bar[0].style, { width: `${(100 / quizModal.pollLength) * (quizModal.currentStep + 1)}%` })
+        $.each(quizModal.pollArr, function (i) {
+          Object.assign(quizModal.pollArr[i].style, { transform: `translateX(-${(quizModal.currentStep * 100)}%)` })
+        })
+        Object.assign(quizModal.heightContainer[0].style, { height: `${quizModal.pollArr[quizModal.currentStep].scrollHeight}px` })
+        ++quizModal.currentStep
+        quizModal.currentStepText.html(quizModal.currentStep)
+        if (quizModal.currentStep == quizModal.maxStep) {
+          quizModal.evtGoNext.html('Show Results')
+        }
+      }
+    } else {
+      if (quizModal.noEmptyAnswers()) {
+        Object.assign(quizModal.heightContainer[0].style, { height: '0px' })
+        Object.assign(quizModal.bar[0].style, { width: '5%' })
+        $.each(quizModal.pollArr, function (i) {
+          Object.assign(quizModal.pollArr[i].style, { transform: `translateX(-${(quizModal.currentStep * 100)}%)` })
+        })
+        Object.assign(quizModal.pollArr[(quizModal.currentStep - 1)].style, { opacity: 0 })
+        quizModal.evtGoNext.prop('disabled', true)
+        quizModal.fullStepText.text('FINDING WATCHES')
+        $('.quiz-close-btn').remove()
+        quizModal.isFinished = true
+        setTimeout(() => {
+          quizModal.bar.animate({ width: '100%' }, 3000, function () { quizModal.close() })
+        }, 600);
+      }
+    }
+  },
+  open: function () {
+    lockScroll()
+    quizModal.modal.show()
+    Object.assign(this.heightContainer[0].style, { height: `${this.getObjectScrollHeight(this.pollArr[quizModal.currentStep - 1])}` })
+    setTimeout(() => {
+      Object.assign(quizModal.backdrop[0].style, { opacity: 1 })
+      Object.assign(quizModal.container[0].style, { transform: 'translateY(0px)', opacity: 1 })
+    }, 1);
+  },
+  close: function () {
+    unlockScroll()
+    let timeToHide = (parseFloat(window.getComputedStyle(quizModal.backdrop[0]).transitionDuration) * 1000)
+    Object.assign(quizModal.backdrop[0].style, { opacity: 0 })
+    Object.assign(quizModal.container[0].style, { transform: 'translateY(32px)', opacity: 0 })
+    setTimeout(() => {
+      quizModal.modal.hide()
+    }, timeToHide);
+  }
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
   initPageObjects();
   initTelInput();
@@ -2930,6 +3053,7 @@ document.addEventListener("DOMContentLoaded", function () {
   attachStickyScroll()
   mailModal.init()
   bfsModal.init()
+  quizModal.init()
 });
 function initValidators() {
   $(".needs-validation").parsley({
