70024383d0a3a48ecc882bf7687436af70bc180d
Давид Суранов <annasteinway1@gmail.com>
2024-02-05 19:47:25 +1000

    results page / vday page / new filters / new card for results page

--- build/js/partial.js
+++ build/js/partial.js
@@ -1892,64 +1892,64 @@ if (typeof productSplide.init === "function") {
 
 
 /* #region  Filter Modal */
-const filterModal = {
-  init: function () {
-    this.renderDOM()
-    this.bindEvents()
-    this.reset()
-  },
-  renderDOM: function () {
-    this._ = $('.filter-modal')
-    this.backdrop = this._.find('.filter-modal__backdrop')
-    this.container = this._.find('.filter-modal__container')
-    this.content = this._.find('.filter-modal__content')
-    this.evtFilter = $('[data-filter-evt]')
-  },
-  bindEvents: function () {
-    this.evtFilter.click(function () {
-      let thisAttr = $(this).attr('data-filter-evt')
-      thisAttr == 'close' ? filterModal.close() : filterModal.open(thisAttr)
-    })
-    $('#filterSort a').click(function () {
-      if (!$(this).hasClass(IS_ACTIVE)) {
-        $(this).addClass(IS_ACTIVE).siblings().removeClass(IS_ACTIVE)
-      }
-    })
-  },
-  open: function (att) {
-    lockScroll()
-    this._.show()
-    setTimeout(() => {
-      this.backdrop.css({ opacity: 1 })
-      this.container.css({ transform: 'translateX(0%)' })
-    }, 1)
-    this.content.hide()
-    switch (att) {
-      case 'openSort':
-        this.content.filter('#filterSort').show()
-        break;
-      case 'openFilter':
-        this.content.filter('#filterFilter').show()
-        break
-      default:
-        break;
-    }
-  },
-  close: function () {
-    unlockScroll()
-    this.backdrop.css({ opacity: 0 })
-    this.container.css({ transform: 'translateX(100%)' })
-    setTimeout(() => {
-      this._.hide()
-    }, getTransitionTime(this.container));
-  },
-  reset: function () {
-    this.backdrop.css({ opacity: 0 })
-    this._.hide()
-    this.container.css({ transform: 'translateX(100%)' })
-    this.content.eq(0).hide()
-  }
-}
+// const filterModal = {
+//   init: function () {
+//     this.renderDOM()
+//     this.bindEvents()
+//     this.reset()
+//   },
+//   renderDOM: function () {
+//     this._ = $('.filter-modal')
+//     this.backdrop = this._.find('.filter-modal__backdrop')
+//     this.container = this._.find('.filter-modal__container')
+//     this.content = this._.find('.filter-modal__content')
+//     this.evtFilter = $('[data-filter-evt]')
+//   },
+//   bindEvents: function () {
+//     this.evtFilter.click(function () {
+//       let thisAttr = $(this).attr('data-filter-evt')
+//       thisAttr == 'close' ? filterModal.close() : filterModal.open(thisAttr)
+//     })
+//     $('#filterSort a').click(function () {
+//       if (!$(this).hasClass(IS_ACTIVE)) {
+//         $(this).addClass(IS_ACTIVE).siblings().removeClass(IS_ACTIVE)
+//       }
+//     })
+//   },
+//   open: function (att) {
+//     lockScroll()
+//     this._.show()
+//     setTimeout(() => {
+//       this.backdrop.css({ opacity: 1 })
+//       this.container.css({ transform: 'translateX(0%)' })
+//     }, 1)
+//     this.content.hide()
+//     switch (att) {
+//       case 'openSort':
+//         this.content.filter('#filterSort').show()
+//         break;
+//       case 'openFilter':
+//         this.content.filter('#filterFilter').show()
+//         break
+//       default:
+//         break;
+//     }
+//   },
+//   close: function () {
+//     unlockScroll()
+//     this.backdrop.css({ opacity: 0 })
+//     this.container.css({ transform: 'translateX(100%)' })
+//     setTimeout(() => {
+//       this._.hide()
+//     }, getTransitionTime(this.container));
+//   },
+//   reset: function () {
+//     this.backdrop.css({ opacity: 0 })
+//     this._.hide()
+//     this.container.css({ transform: 'translateX(100%)' })
+//     this.content.eq(0).hide()
+//   }
+// }
 /* #endregion */
 
 
@@ -2841,7 +2841,7 @@ const initPageObjects = () => {
     homePageSplide,
     pgModal,
     productPage,
-    filterModal,
+    // filterModal,
     footer,
     pageEls,
     myBag,
@@ -3635,7 +3635,6 @@ const adjustStickyEls = () => {
 
   if (elsArr.length && header) {
     function adjust() {
-      console.log('a')
       let headerHeight = parseInt(window.getComputedStyle(header).getPropertyValue('height'))
       elsArr.forEach((el) => {
         let topValue = $(window).width() > 991 ? headerHeight + 24 : headerHeight
@@ -3861,6 +3860,240 @@ const tagPreview = {
     }
   }
 }
+
+const setFiltersStickyPos = () => {
+  const pageFilters = document.querySelector('.page-filters'),
+    header = document.querySelector('.header')
+
+  if (pageFilters !== null && header !== null) {
+    function adjust() {
+      try {
+        let headerHeight = parseInt(window.getComputedStyle(header).getPropertyValue('height'))
+        pageFilters.style.top = `${headerHeight}px`
+      } catch {
+        console.log('page filters err')
+      }
+    }
+    ['load', 'resize'].forEach((event) => { window.addEventListener(event, () => { adjust() }) })
+  }
+}
+
+const setPageFilters = () => {
+  const filters = Array.from(document.querySelectorAll('.page-filter-box')).reduce((acc, box) => {
+    if (box.querySelector('.page-filter-drop') !== null && box.querySelector('.page-filter-btn') !== null) { acc.push(box) }
+    return acc
+  }, [])
+
+  if (filters.length !== 0) {
+    window.pageFilters = {}
+    window.filterModal = {}
+
+    filterModal = {
+      el: document.querySelector('.filter-modal'),
+      backdrop: document.querySelector('.filter-modal__backdrop'),
+      container: document.querySelector('.filter-modal__container'),
+      title: document.querySelector('.filter-modal-title'),
+      content: document.querySelector('.filter-modal__content'),
+      evtClose: document.querySelectorAll('[data-filter-evt="close"]'),
+      open: function () {
+        lockScroll()
+        if (this.el !== undefined) {
+          this.el.style.display = 'block'
+          setTimeout(() => {
+            this.backdrop.style.opacity = 1
+            this.container.classList.add(IS_VISIBLE)
+          }, 1);
+        }
+      },
+      close: function () {
+        unlockScroll()
+        if (this.el !== undefined) {
+          this.backdrop.style.opacity = 0
+          this.container.classList.remove(IS_VISIBLE)
+          setTimeout(() => {
+            this.el.style.display = 'none'
+          }, getTransitionTime(this.backdrop));
+        }
+      },
+      isHidden: function () {
+        return window.getComputedStyle(this.el).getPropertyValue('display') == 'none'
+      },
+      bindEvents: function () {
+        this.evtClose.forEach((el) => {
+          el.onclick = () => {
+            const labels = [...filterModal.content.querySelectorAll('label')]
+            const links = [...filterModal.content.querySelectorAll('a')]
+            const emptyBox = document.querySelector('.page-filter-drop.is-empty')
+            filterModal.close()
+            setTimeout(() => {
+              if (emptyBox !== null) {
+                const toMove = links.length !== 0 ? links : labels
+                toMove.forEach((node) => { emptyBox.querySelector('div').appendChild(node) })
+                emptyBox.classList.remove(IS_EMPTY)
+              }
+            }, getTransitionTime(filterModal.container));
+          }
+        })
+      },
+      appendLabels: function (nodes) {
+        this.content.appendChild(nodes)
+      }
+    }
+    filterModal.bindEvents()
+
+
+    filters.forEach((filter, i) => {
+      const drop = filter.querySelector('.page-filter-drop')
+      const trigger = filter.querySelector('.page-filter-btn')
+      const labels = [...drop.querySelectorAll('label')]
+      const buttons = [...drop.querySelectorAll('label > div')]
+      const inputs = [...drop.querySelectorAll('input')]
+      const links = [...drop.querySelectorAll('a')]
+
+      pageFilters[i] = {
+        drop: drop,
+        trigger: trigger,
+        labels: labels,
+        buttons: buttons,
+        inputs: inputs,
+        links: links
+      }
+      pageFilters[i].drop.show = function () {
+        this.style.display = 'block'
+        setTimeout(() => {
+          this.style.opacity = 1
+          this.style.transform = 'translateY(0px)'
+        }, 1);
+      }
+      pageFilters[i].drop.hide = function () {
+        this.style.opacity = 0
+        this.style.transform = 'translateY(10px)'
+        setTimeout(() => {
+          this.style.display = 'none'
+        }, getTransitionTime(this));
+      }
+      pageFilters[i].drop.isHidden = function () {
+        return window.getComputedStyle(this).getPropertyValue('display') == 'none'
+      }
+    })
+
+    pageFilters.observeCheckState = function () {
+      for (const key in this) {
+        const obj = this[key]
+        if (typeof obj !== 'function') {
+          if (obj.inputs.some(input => input.checked)) {
+            obj.trigger.classList.add(IS_ACTIVE)
+          } else {
+            obj.trigger.classList.remove(IS_ACTIVE)
+          }
+        }
+      }
+    }
+    pageFilters.attachInputOnChange = function () {
+      for (const key in this) {
+        const obj = this[key]
+        if (typeof obj !== 'function') {
+          obj.inputs.forEach((input) => {
+            input.onchange = () => {
+              pageFilters.observeCheckState()
+            }
+          })
+        }
+      }
+    }
+    pageFilters.atLeastOneVisible = function () {
+      for (const key in this) {
+        if (typeof this[key] !== 'function') {
+          if (!this[key].drop.isHidden()) return true
+        }
+      }
+    }
+    pageFilters.showAll = function () {
+      for (const key in this) {
+        const obj = this[key]
+        if (typeof obj !== 'function') {
+          obj.drop.show()
+        }
+      }
+    }
+    pageFilters.hideAll = function (node) {
+      for (const key in this) {
+        const obj = this[key]
+        if (typeof obj !== 'function') {
+          if (node !== null && node !== undefined) {
+            if (!obj.drop.isSameNode(node)) { obj.drop.hide() }
+          } else {
+            obj.drop.hide()
+          }
+        }
+      }
+    }
+    pageFilters.attachTriggerClick = function () {
+      for (const key in this) {
+        const obj = this[key]
+        if (typeof obj !== 'function') {
+          obj.trigger.onclick = () => {
+            if (window.innerWidth > 991) {
+              if (obj.drop.isHidden()) {
+                pageFilters.hideAll(obj.drop)
+                obj.drop.show()
+              } else {
+                obj.drop.hide()
+              }
+            } else {
+              const title = obj.trigger.querySelector('span').innerHTML
+              filterModal.open()
+              filterModal.title.innerHTML = title
+              obj.drop.classList.add(IS_EMPTY)
+              const toMove = obj.links.length !== 0 ? obj.links : obj.labels
+              toMove.forEach((node) => { filterModal.appendLabels(node) })
+            }
+          }
+        }
+      }
+    }
+    pageFilters.attachWindowEvents = function () {
+      document.addEventListener('click', function (event) {
+        const target = event.target
+        if (!target.closest('.page-filter-box') && pageFilters.atLeastOneVisible()) {
+          pageFilters.hideAll()
+        }
+      })
+      window.addEventListener('scroll', function () {
+        if (pageFilters.atLeastOneVisible()) {
+          pageFilters.hideAll()
+        }
+      })
+    }
+    pageFilters.init = function () {
+      pageFilters.observeCheckState()
+      pageFilters.attachInputOnChange()
+      pageFilters.attachTriggerClick()
+      pageFilters.attachWindowEvents()
+    }
+
+    try {
+      pageFilters.init()
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+}
+
+const bindCardFav = () => {
+  const buttons = document.querySelectorAll('.card-item-fav')
+  buttons.forEach(btn => btn.onclick = () => {
+    if (btn.classList.contains(IS_ACTIVE)) {
+      btn.classList.remove(IS_ACTIVE)
+    } else {
+      btn.classList.add(IS_ACTIVE)
+    }
+  })
+}
+
+setPageFilters()
+setFiltersStickyPos()
+bindCardFav()
 
 
 document.addEventListener("DOMContentLoaded", function () {
