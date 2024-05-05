13a06d09e9152dce4997f959e23b38a67cd09cd1
Давид Суранов <annasteinway1@gmail.com>
2024-05-05 13:23:20 +0700

    price gold page --admin

--- src/js/admin/main.js
+++ src/js/admin/main.js
@@ -2864,3 +2864,51 @@ document.addEventListener('DOMContentLoaded', () => {
     POS.init()
   }
 })
+
+class GoldPrice {
+  constructor() {
+    this.table = document.querySelector('[data-tb="gold_prices"]')
+  }
+
+  updateChanges() {
+    if (this.table) {
+      const rows = [...this.table.querySelectorAll('[data-row]')]
+      rows.forEach((row, index, arr) => {
+        const priceEl = row.querySelector('[data-price]')
+        const prevPriceEl = arr[index + 1] ? arr[index + 1].querySelector('[data-price]') : null
+        const changeEl = row.querySelector('[data-change]')
+
+        if (priceEl && changeEl && prevPriceEl) {
+          const price = Number(priceEl.innerHTML.replace(/[^0-9.]/g, ''))
+          const prevPrice = Number(prevPriceEl.innerHTML.replace(/[^0-9.]/g, ''))
+          const change = (price - prevPrice).toFixed(2)
+          const percentChange = ((change / prevPrice) * 100).toFixed(2)
+          const sign = change > 0 ? '+' : '-'
+          const resultHTML = `${sign} ${percentChange.replace(/\+|-/g, '')}% / ${sign} $${change.replace(/\+|-/g, '')}`
+          changeEl.innerHTML = resultHTML
+
+          if (change < 0) {
+            changeEl.classList.add('--negative')
+            priceEl.classList.add('--negative')
+          } else {
+            priceEl.classList.add('--positive')
+          }
+        } else {
+          changeEl.classList.add('--na')
+        }
+      })
+    }
+  }
+
+  init() {
+    this.updateChanges()
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const goldMain = document.querySelector('.main_gold')
+  if (goldMain) {
+    const goldPricePage = new GoldPrice()
+    goldPricePage.init()
+  }
+})
