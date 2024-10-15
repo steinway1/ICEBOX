a7b7bf4ecf7bc7fc8dbff352df5fc122c29fd702
Давид Суранов <annasteinway1@gmail.com>
2024-10-15 23:56:08 +1000

    Track orde rpage update

--- src/js/modules/elements/page-elements.js
+++ src/js/modules/elements/page-elements.js
@@ -11,6 +11,54 @@ const pageEls = new Object({
     })
   },
   attachEvent: {
+    // Track Order
+    trackOrderSwitch: () => {
+      const arr = document.querySelectorAll('input[name="track_with"]')
+      if (arr.length) {
+
+        function toggleInput(value) {
+          const elements = {
+            phone: document.querySelector('#form_track_order .iti'),
+            email: document.querySelector('#input_track_email')
+          }
+
+          if (!elements.phone || !elements.email) {
+            console.error('phone or email not found')
+            return
+          }
+
+          if (elements[value]) {
+            Object.keys(elements).forEach(key => {
+              elements[key].style.display = key === value ? 'block' : 'none'
+            })
+          } else {
+            console.error(`Invalid value: ${value}`)
+          }
+        }
+
+        arr.forEach((input) => {
+          input.addEventListener('change', () => {
+            toggleInput(input.value)
+          })
+        })
+      }
+    },
+    resetTrackOrderForm: () => {
+      const elem = document.querySelector('[data-evt="reset_track_form"]')
+      if (elem) {
+        elem.addEventListener('click', (e) => {
+          e.preventDefault()
+          const form = document.querySelector('#form_track_order')
+          if (!form) {
+            console.error('form not found')
+            return
+          }
+
+          form.reset()
+        })
+      }
+    },
+    // Other
     filterDropdown: () => {
       let dropdownEls = Array.from($('.filter-dropdown'))
 
