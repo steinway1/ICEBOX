66cb5f591cd81b6ef22e2ee0faa105d13f772f00
Давид Суранов <annasteinway1@gmail.com>
2024-10-26 02:06:59 +1000

    25 Oct

--- src/js/modules/elements/footer.js
+++ src/js/modules/elements/footer.js
@@ -1,40 +1,22 @@
 const footer = new Object({
-  init: function () {
-    function attach() {
-      if (window.innerWidth < 992) {
-        let footerHeader = Array.from($(".footer-col__header"));
+  bindEvents: function() {
+    if (window.innerWidth < 480) {
+      const evtArr = [...document.querySelectorAll('.footer__col-head')]
+      for (const elem of evtArr) {
+        const col = elem.closest('.footer__col')
+        if (!col) return
 
-        $.each(footerHeader, function (i) {
-          let col = $(footerHeader[i]).closest(".footer-col"),
-            body = col.find(".footer-col__body");
-
-          if (body.length && col.length) {
-            footerHeader[i].onclick = () => {
-              if (body.height() == 0) {
-                let scrH = body.find(".footer-col__body-scroll")[0]
-                  .scrollHeight;
-                body.css({
-                  height: `${scrH}px`,
-                });
-              } else {
-                let h = body.css("height");
-                body.css("height", h);
-                setTimeout(() => {
-                  body.css({
-                    height: "0px",
-                  });
-                }, 1);
-              }
-            };
-          }
-        });
+        if (!col.classList.contains('.--static')) {
+          elem.addEventListener('click', () => {
+            col.classList.toggle('--open')
+          })
+        }
       }
     }
-
-    $(window).on("load resize", function () {
-      attach();
-    });
   },
+  init: function () {
+    this.bindEvents()
+  }
 })
 
 module.exports = footer
