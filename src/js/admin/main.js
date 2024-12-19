9d83b311ef1555ffef149d61883ecf0c62ac025e
Давид Суранов <annasteinway1@gmail.com>
2024-12-20 01:47:53 +1000

    Inventory Admin Sort and Filters changed

--- src/js/admin/main.js
+++ src/js/admin/main.js
@@ -4893,16 +4893,16 @@ document.addEventListener('DOMContentLoaded', () => {
  * @type {class}
  */
 class Inventory {
-  constructor() { 
+  constructor() {
     this.rootEl = document.querySelector('.main_inventory')
     if (!this.rootEl) return
 
     this.init()
-   }
+  }
 
-   init() {
+  init() {
     this.initSplide()
-   }
+  }
 
   // Methods
   initSplide() {
@@ -4921,4 +4921,134 @@ class Inventory {
 
 document.addEventListener('DOMContentLoaded', () => {
   new Inventory()
+})
+
+/**
+ * PS Select
+ */
+
+class PsSelect {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.attachPs()
+    this.bindClickPs()
+  }
+
+  // Events
+  getArr() {
+    return [...document.querySelectorAll('.ps')]
+  }
+  getOpenedElements() {
+    return [...document.querySelectorAll('.ps.--active')]
+  }
+
+
+  // Methods
+  attachPs() {
+    for (const ps of this.getArr()) {
+      const drop = ps.querySelector('.ps-drop')
+      const scroller = drop.querySelector('.ps-drop__scroller')
+      const current = ps.querySelector('[data-ps-current]')
+      const inputArr = [...ps.querySelectorAll('input')]
+
+      if (ps.initialized) {
+        return
+      }
+
+      if (!drop) {
+        console.warn('PS SELECT: Drop not found')
+        return
+      }
+
+      if (!scroller) {
+        console.warn('PS SELECT: Scroller not found')
+        return
+      }
+
+
+      ps.initialized = true
+
+      ps.open = () => {
+        const openedArr = this.getOpenedElements()
+        for (const opened of openedArr) {
+          opened.close()
+        }
+
+        ps.opened = true
+        ps.classList.add('--active')
+      }
+
+      ps.close = () => {
+        ps.opened = false
+        ps.classList.remove('--active')
+      }
+
+      ps.update = () => {
+        if (inputArr.length && current) {
+          for (const input of inputArr) {
+            if (input.checked) {
+              const label = input.closest('label')
+              const caption = label.querySelector('.ps-caption')
+              const counter = label.querySelector('.ps-counter')
+
+              let text = ''
+
+              if (caption) {
+                text = caption.textContent
+              }
+
+              if (counter) {
+                text += ` — ${counter.textContent}`
+              }
+
+              current.textContent = text
+              break
+            }
+          }
+        }
+      }
+
+      inputArr.forEach((input) => {
+        input.addEventListener('change', () => {
+          ps.update()
+        })
+      })
+
+      ps.update()
+    }
+  }
+
+
+  // Attach / Bind
+  bindClickPs() {
+    for (const ps of this.getArr()) {
+      const btn = ps.querySelector('.ps-btn')
+      btn.addEventListener('click', () => {
+        if (ps.opened) {
+          ps.close()
+          return
+        } else {
+          ps.open()
+        }
+      })
+    }
+
+    document.addEventListener('click', (e) => {
+      const t = e.target
+      if (t.closest('.ps') === null) {
+        for (const ps of this.getArr()) {
+          if (ps.opened) {
+            ps.close()
+          }
+        }
+      }
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.psSelect = new PsSelect()
 })
