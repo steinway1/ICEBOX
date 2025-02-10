ea0932735ba293d0f8f89be80c32d54122becb49
Давид Суранов <annasteinway1@gmail.com>
2025-02-10 23:20:34 +1000

    Grid / Row view - Manufacture

--- src/js/admin/modules/pages/manufacture.js
+++ src/js/admin/modules/pages/manufacture.js
@@ -1,4 +1,5 @@
 import ManufactureGrid from "../components/manufacture/manufacture-grid";
+import { IS_ACTIVE } from "../general/constants";
 
 export default class Manufacture {
   /**
@@ -16,10 +17,33 @@ export default class Manufacture {
 
   init() {
     this.initGrids();
+    this.bindViewSwitch();
   }
   initGrids() {
     for (const type of this.#GridTypesToRender) {
       new ManufactureGrid(type);
     }
+  }
+
+  // Events
+  bindViewSwitch() {
+		const viewSwitchElements = document.querySelectorAll('[data-view]');
+
+		viewSwitchElements.forEach((view) => {
+			view.addEventListener('click', () => {
+				viewSwitchElements.forEach((el) => el.classList.remove(IS_ACTIVE));
+				view.classList.add(IS_ACTIVE);
+
+				const { view: viewType } = view.dataset;
+	
+				document.body.classList.remove('--grid', '--row');
+	
+				if (viewType === 'grid') {
+					document.body.classList.add('--grid');
+				} else if (viewType === 'rows') {
+          document.body.classList.add("--row");
+        }
+      });
+    });
   }
 }
