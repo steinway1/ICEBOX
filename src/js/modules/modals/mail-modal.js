bee00a39b3e55ad587f32f91907729acdc8411cd
steinway1 <annasteinway1@gmail.com>
2025-03-21 01:30:41 +1000

    required email changed to required phone -- cart modal when add to cart

--- src/js/modules/modals/mail-modal.js
+++ src/js/modules/modals/mail-modal.js
@@ -1,43 +1,45 @@
 const mailModal = new Object({
   object_to_render: null,
   init: function () {
-    this.renderDOM()
+    this.renderDOM();
     if (this.modal.length) {
-      this.bindEvents()
+      this.bindEvents();
     }
   },
   renderDOM: function () {
-    this.modal = $('.mail-modal')
-    this.backdrop = $('.mail-modal__backdrop')
-    this.container = $('.mail-modal__container')
-    this.evtClose = $('[data-mail-modal="close"]')
+    this.modal = $(".mail-modal");
+    this.backdrop = $(".mail-modal__backdrop");
+    this.container = $(".mail-modal__container");
+    this.evtClose = $('[data-mail-modal="close"]');
   },
   bindEvents: function () {
-    this.evtClose.on('click', function () { mailModal.close() })
+    this.evtClose.on("click", function () {
+      mailModal.close();
+    });
   },
   open: function (object) {
-    lockScroll()
-    this.modal.show()
-    this.modal.find('input').focus()
+    lockScroll();
+    this.modal.show();
+    this.modal.find("input").focus();
     setTimeout(() => {
-      this.modal.addClass(__ACTIVE)
+      this.modal.addClass(__ACTIVE);
     }, 5);
 
     if (object) {
-      this.object_to_render = object
+      this.object_to_render = object;
     }
   },
   close: function () {
-    unlockScroll()
-    this.modal.removeClass(__ACTIVE)
+    unlockScroll();
+    this.modal.removeClass(__ACTIVE);
     setTimeout(() => {
-      this.modal.hide()
+      this.modal.hide();
     }, getTransitionTime(this.container));
 
     if (this.object_to_render) {
-      window.addCartModal.create(this.object_to_render)
+      window.addCartModal.create(this.object_to_render);
     }
-  }
-})
+  },
+});
 
-module.exports = mailModal
+module.exports = mailModal;
