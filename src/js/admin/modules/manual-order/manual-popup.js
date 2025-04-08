efca8f27921be9291a9ee3b7b497257bcb7c2d13
steinway1 <annasteinway1@gmail.com>
2025-04-08 20:44:11 +1000

    Admin : Add Transactions - Add Required fields

--- src/js/admin/modules/manual-order/manual-popup.js
+++ src/js/admin/modules/manual-order/manual-popup.js
@@ -1,147 +1,145 @@
-import ManualOrderForm from './manual-form'
+import ManualOrderForm from "./manual-form";
 
 import {
-	lockScroll,
-	unlockScroll,
-	delay,
-	getTransitionTime
-} from '../general/utils'
+  lockScroll,
+  unlockScroll,
+  delay,
+  getTransitionTime,
+} from "../general/utils";
 
-import {
-	__VISIBLE,
-	__HIDDEN
-} from '../general/constants'
+import { __VISIBLE, __HIDDEN } from "../general/constants";
 
-import PopupBackdrop from '../dynamic/popup-backdrop'
-
+import PopupBackdrop from "../dynamic/popup-backdrop";
 
 export default class ManualOrderPopup {
-	constructor(id = null) {
-		this.rootEl = document.querySelector('#addManulOrderPopup')
-		if (!this.rootEl) return
+  constructor(id = null) {
+    this.rootEl = document.querySelector("#addManulOrderPopup");
+    if (!this.rootEl) return;
 
-		this.orderID = id
-		this.handler = this.rootEl.querySelector('.m-popup__handler')
-		this.opened = false
-		this.formInstance = null
-		this.init()
-	}
-	init() {
-		this.formInstance = new ManualOrderForm(this.rootEl, this, this.orderID)
-		this.bindEvents()
-		this.show()
-	}
+    this.orderID = id;
+    this.handler = this.rootEl.querySelector(".m-popup__handler");
+    this.opened = false;
+    this.formInstance = null;
+    this.init();
+  }
+  init() {
+    this.formInstance = new ManualOrderForm(this.rootEl, this, this.orderID);
+    this.bindEvents();
+    this.show();
+  }
 
-	get getInstance() {
-		return this.rootEl
-	}
+  get getInstance() {
+    return this.rootEl;
+  }
 
-	// Methods
-	reset() {
-		return
-	}
-	async show() {
-		if (this.opened) {
-			return
-		}
+  // Methods
+  reset() {
+    return;
+  }
+  async show() {
+    if (this.opened) {
+      return;
+    }
 
-		lockScroll()
-		this.opened = true
-		this.rootEl.style.display = 'block'
-		this.formInstance._bindDocumentClick()
-		this.rootEl.scrollTop = 0
+    lockScroll();
+    this.opened = true;
+    this.rootEl.style.display = "block";
+    this.formInstance._bindDocumentClick();
+    this.rootEl.scrollTop = 0;
 
-		await delay(1)
+    await delay(1);
 
-		this.rootEl.classList.add(__VISIBLE)
-		window.MPopupBackdrop = new PopupBackdrop({
-			callback: () => { this.close() }
-		})
-	}
-	async close() {
-		if (!this.opened) {
-			return
-		}
+    this.rootEl.classList.add(__VISIBLE);
+    window.MPopupBackdrop = new PopupBackdrop({
+      callback: () => {
+        this.close();
+      },
+    });
+  }
+  async close() {
+    if (!this.opened) {
+      return;
+    }
 
-		unlockScroll()
-		this.opened = false
-		this.rootEl.classList.remove(__VISIBLE)
-		this.rootEl.classList.add(__HIDDEN)
-		this.formInstance._unbindDocumentClick()
+    unlockScroll();
+    this.opened = false;
+    this.rootEl.classList.remove(__VISIBLE);
+    this.rootEl.classList.add(__HIDDEN);
+    this.formInstance._unbindDocumentClick();
 
-		if (window.MPopupBackdrop) {
-			window.MPopupBackdrop.hide(true)
-			delete window.MPopupBackdrop
-		}
+    if (window.MPopupBackdrop) {
+      window.MPopupBackdrop.hide(true);
+      delete window.MPopupBackdrop;
+    }
 
-		if (window.manualOrderPopup) {
-			delete window.manualOrderPopup
-		}
+    if (window.manualOrderPopup) {
+      delete window.manualOrderPopup;
+    }
 
-		await delay(getTransitionTime(this.rootEl))
+    await delay(getTransitionTime(this.rootEl));
 
-		this.rootEl.removeAttribute('style')
-		this.rootEl.classList.remove(__HIDDEN)
-		this.formInstance.fullReset()
-	}
+    this.rootEl.removeAttribute("style");
+    this.rootEl.classList.remove(__HIDDEN);
+    this.formInstance.fullReset();
+  }
 
-	// Events
-	bindEvents() {
-		document.addEventListener('click', (e) => {
-			const target = e.target
+  // Events
+  bindEvents() {
+    document.addEventListener("click", (e) => {
+      const target = e.target;
 
-			// Close
-			if (target.closest('[data-m-popup="close"]')) {
-				this.hide()
-			}
+      // Close
+      if (target.closest('[data-m-popup="close"]')) {
+        this.hide();
+      }
 
-			// Submit
-			if (target.closest('[data-m-popup="submit"]')) {
-				this.submit()
-			}
-		})
-	}
-	bindDragEvents() {
-		if (window.innerWidth < 992) {
-			const handler = this.handler
-			const container = this.rootEl
+      // Submit
+      if (target.closest('[data-m-popup="submit"]')) {
+        this.submit();
+      }
+    });
+  }
+  bindDragEvents() {
+    if (window.innerWidth < 992) {
+      const handler = this.handler;
+      const container = this.rootEl;
 
-			let startY = 0
-			let currentY = 0
-			// let containerHeight = container.offsetHeight
-			let isDragging = false
+      let startY = 0;
+      let currentY = 0;
+      // let containerHeight = container.offsetHeight
+      let isDragging = false;
 
-			container.addEventListener('touchstart', (e) => {
-				startY = e.touches[0].clientY
-				isDragging = true
-				container.style.transition = 'none'
-			})
+      container.addEventListener("touchstart", (e) => {
+        startY = e.touches[0].clientY;
+        isDragging = true;
+        container.style.transition = "none";
+      });
 
-			container.addEventListener('touchmove', (e) => {
-				if (!isDragging) return
+      container.addEventListener("touchmove", (e) => {
+        if (!isDragging) return;
 
-				currentY = e.touches[0].clientY
-				let diffY = currentY - startY
+        currentY = e.touches[0].clientY;
+        let diffY = currentY - startY;
 
-				if (diffY > 0) {
-					container.style.transform = `translateY(${diffY}px)`
-				}
-			})
+        if (diffY > 0) {
+          container.style.transform = `translateY(${diffY}px)`;
+        }
+      });
 
-			container.addEventListener('touchend', (e) => {
-				if (!isDragging) return
-				isDragging = false
+      container.addEventListener("touchend", (e) => {
+        if (!isDragging) return;
+        isDragging = false;
 
-				let diffY = currentY - startY
-				let hideOffset = container.offsetHeight * 0.5
-				container.style.transition = 'all .3s cubic-bezier(.39, .575, .565, 1)'
+        let diffY = currentY - startY;
+        let hideOffset = container.offsetHeight * 0.5;
+        container.style.transition = "all .3s cubic-bezier(.39, .575, .565, 1)";
 
-				if (diffY > hideOffset) {
-					this.rootEl.close()
-				} else {
-					container.style.transform = `translateY(0%)`
-				}
-			})
-		}
-	}
+        if (diffY > hideOffset) {
+          this.rootEl.close();
+        } else {
+          container.style.transform = `translateY(0%)`;
+        }
+      });
+    }
+  }
 }
--- src/js/admin/modules/manual-order/validator.js
+++ src/js/admin/modules/manual-order/validator.js
@@ -1,120 +1,176 @@
 class ItemValidator {
-	constructor(contentElem) {
-		this.contentElem = contentElem;
-	}
-	validate() {
-		const itemTitleInput = this.contentElem.querySelector('input[name="item_title"]')
-		const selectArr = [...this.contentElem.querySelectorAll('select[required]')]
+  constructor(contentElem) {
+    this.contentElem = contentElem;
+  }
+  validate() {
+    const itemTitleInput = this.contentElem.querySelector(
+      'input[name="item_title"]'
+    );
+    const selectArr = [
+      ...this.contentElem.querySelectorAll("select[required]"),
+    ];
 
-		if (itemTitleInput) {
-			if (!itemTitleInput.value) {
-				return { result: false, msg: 'Item title is required.' };
-			}
-		} else {
-			return { result: false, msg: 'No item. Please use search field to find an item.' };
-		}
+    if (itemTitleInput) {
+      if (!itemTitleInput.value) {
+        return { result: false, msg: "Item title is required." };
+      }
+    } else {
+      return {
+        result: false,
+        msg: "No item. Please use search field to find an item.",
+      };
+    }
 
-		if (selectArr && selectArr.length) {
-			if (selectArr.every(select => !select.value)) {
-				return {
-					result: false,
-					msg: 'Select product options...'
-				}
-			}
-			const firstNotSelected = selectArr.find(select => !select.value)
-			if (firstNotSelected) {
-				return {
-					result: false,
-					msg: `Select option: ${firstNotSelected.getAttribute('name')}`
-				}
-			}
-		}
+    if (selectArr && selectArr.length) {
+      if (selectArr.every((select) => !select.value)) {
+        return {
+          result: false,
+          msg: "Select product options...",
+        };
+      }
+      const firstNotSelected = selectArr.find((select) => !select.value);
+      if (firstNotSelected) {
+        return {
+          result: false,
+          msg: `Select option: ${firstNotSelected.getAttribute("name")}`,
+        };
+      }
+    }
 
-		return { result: true, msg: '' };
-	}
+    return { result: true, msg: "" };
+  }
 }
 class CustomerValidator {
-	constructor(contentElem) {
-		this.contentElem = contentElem;
-	}
-	_validateEmail(emailString) {
-		const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		return emailPattern.test(emailString);
-	}
+  constructor(contentElem) {
+    this.contentElem = contentElem;
+  }
+  _validateEmail(emailString) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(emailString);
+  }
 
-	validate() {
-		const customerInputArr = [...this.contentElem.querySelectorAll('input[required][data-customer-input]')]
-		const emailInput = this.contentElem.querySelector('input[name="email"][required][data-customer-input]')
+  validate() {
+    const customerInputArr = [
+      ...this.contentElem.querySelectorAll(
+        "input[required][data-customer-input]"
+      ),
+    ];
+    const emailInput = this.contentElem.querySelector(
+      'input[name="email"][required][data-customer-input]'
+    );
 
-		if (!customerInputArr || !customerInputArr.length) {
-			return { result: false, msg: 'Something went wrong. ERR CODE: VMO79' };
-		}
+    if (!customerInputArr || !customerInputArr.length) {
+      return { result: false, msg: "Something went wrong. ERR CODE: VMO79" };
+    }
 
-		if (customerInputArr.every(input => !input.value)) {
-			return { result: false, msg: 'Enter customer details or use search field to find a customer.' };
-		}
+    if (customerInputArr.every((input) => !input.value)) {
+      return {
+        result: false,
+        msg: "Enter customer details or use search field to find a customer.",
+      };
+    }
 
-		const firstNotFilled = customerInputArr.find(input => !input.value)
-		if (firstNotFilled) {
-			return { result: false, msg: `Enter customer ${firstNotFilled.getAttribute('placeholder')}.` };
-		}
+    const firstNotFilled = customerInputArr.find((input) => !input.value);
+    if (firstNotFilled) {
+      return {
+        result: false,
+        msg: `Enter customer ${firstNotFilled.getAttribute("placeholder")}.`,
+      };
+    }
 
-		if (emailInput) {
-			if (!this._validateEmail(emailInput.value)) {
-				return { result: false, msg: 'Enter a valid email address.' };
-			}
-		}
+    if (emailInput) {
+      if (!this._validateEmail(emailInput.value)) {
+        return { result: false, msg: "Enter a valid email address." };
+      }
+    }
 
-		return { result: true, msg: '' };
-	}
+    return { result: true, msg: "" };
+  }
 }
 class OtherDetailsValidator {
-	constructor(contentElem) {
-		this.contentElem = contentElem;
-	}
+  constructor(contentElem) {
+    this.contentElem = contentElem;
+  }
 
-	validate() {
-		const requiredSelectArr = [...this.contentElem.querySelectorAll('select[required]')]
-		const requiredInputArr = [...this.contentElem.querySelectorAll('input[required]')]
-		const dateInput = this.contentElem.querySelector('input[name="order_date"][required]')
+  validate() {
+    const requiredSelectArr = [
+      ...this.contentElem.querySelectorAll("select[required]"),
+    ];
+    const requiredInputArr = [
+      ...this.contentElem.querySelectorAll("input[required]"),
+    ];
+    const dateInput = this.contentElem.querySelector(
+      'input[name="order_date"][required]'
+    );
 
-		if (requiredSelectArr.every(select => !select.value) && requiredInputArr.every(input => !input.value)) {
-			return { result: false, msg: 'Enter order details.' };
-		}
+    if (
+      requiredSelectArr.every((select) => !select.value) &&
+      requiredInputArr.every((input) => !input.value)
+    ) {
+      return { result: false, msg: "Enter order details." };
+    }
 
-		if (dateInput && !dateInput.value) {
-			return { result: false, msg: 'Order date is not filled.' };
-		}
+    if (dateInput && !dateInput.value) {
+      return { result: false, msg: "Order date is not filled." };
+    }
 
-		const firstNotSelected = requiredSelectArr.find(select => !select.value)
-		if (firstNotSelected) {
-			return { result: false, msg: `Select option: ${firstNotSelected.getAttribute('name')}` };
-		}
+    const firstNotSelected = requiredSelectArr.find((select) => !select.value);
+    if (firstNotSelected) {
+      return {
+        result: false,
+        msg: `Select option: ${firstNotSelected.getAttribute("name")}`,
+      };
+    }
 
-		const firstNotFilled = requiredInputArr.find(input => !input.value)
-		if (firstNotFilled) {
-			return { result: false, msg: `Enter ${firstNotFilled.getAttribute('placeholder')}` };
-		}
+    const firstNotFilled = requiredInputArr.find((input) => !input.value);
+    if (firstNotFilled) {
+      return {
+        result: false,
+        msg: `Enter ${firstNotFilled.getAttribute("placeholder")}`,
+      };
+    }
 
-		return { result: true, msg: '' };
-	}
+    const psRequired = [
+      ...this.contentElem.querySelectorAll(
+        "div[data-ps-multiple][data-ps-required]"
+      ),
+    ];
+    if (psRequired.length) {
+      const invalidPs = psRequired.find((ps) => {
+        const current = ps.querySelector("[data-ps-selected]");
+        return current && current.textContent.toLowerCase().includes("empty");
+      });
+
+      if (invalidPs) {
+        return {
+          result: false,
+          msg: `Please select at least one payment method.`,
+        };
+      }
+    }
+
+    return { result: true, msg: "" };
+  }
 }
 export default class ManualOrderValidator {
-	constructor(contentType, contentElem) {
-		this.validatorStrategy = this.getValidatorStrategy(contentType, contentElem)
-	}
-	getValidatorStrategy(contentType, contentElem) {
-		const strategies = {
-			item: new ItemValidator(contentElem),
-			customer: new CustomerValidator(contentElem),
-			other: new OtherDetailsValidator(contentElem)
-		}
-		return strategies[contentType] || null
-	}
-	run() {
-		if (!this.validatorStrategy) {
-			return { result: false, msg: 'ERR: VMO76. Invalid content type' }
-		}
-		return this.validatorStrategy.validate()
-	}
+  constructor(contentType, contentElem) {
+    this.validatorStrategy = this.getValidatorStrategy(
+      contentType,
+      contentElem
+    );
+  }
+  getValidatorStrategy(contentType, contentElem) {
+    const strategies = {
+      item: new ItemValidator(contentElem),
+      customer: new CustomerValidator(contentElem),
+      other: new OtherDetailsValidator(contentElem),
+    };
+    return strategies[contentType] || null;
+  }
+  run() {
+    if (!this.validatorStrategy) {
+      return { result: false, msg: "ERR: VMO76. Invalid content type" };
+    }
+    return this.validatorStrategy.validate();
+  }
 }
