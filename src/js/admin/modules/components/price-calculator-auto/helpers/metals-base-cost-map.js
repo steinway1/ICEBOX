4805bc313994add24e0b2ca0ff7e0dbea6e433f4
steinway1 <annasteinway1@gmail.com>
2025-05-08 21:23:11 +1000

    Price calculator added 24K gold

--- src/js/admin/modules/components/price-calculator-auto/helpers/metals-base-cost-map.js
+++ src/js/admin/modules/components/price-calculator-auto/helpers/metals-base-cost-map.js
@@ -1,15 +1,16 @@
 const BASE_PRICE_SELECTOR = {
-  gold: "input#baseGoldPrice",
-  silver: "input#baseSilverPrice",
-  platinum: "input#basePlatinumPrice",
+  gold: 'input#baseGoldPrice',
+  silver: 'input#baseSilverPrice',
+  platinum: 'input#basePlatinumPrice',
 };
 
 const METAL_SPECS = {
-  "10K": { metal: "gold", spotMarkup: 0.01 },
-  "14K": { metal: "gold", spotMarkup: 0.01 },
-  "18K": { metal: "gold", spotMarkup: 0.01 },
-  S925: { metal: "silver", spotMarkup: 0.1, correction: 1 / 0.999 },
-  P950: { metal: "platinum", spotMarkup: 0.1, correction: 1 / 0.999 },
+  '10K': { metal: 'gold', spotMarkup: 0.01 },
+  '14K': { metal: 'gold', spotMarkup: 0.01 },
+  '18K': { metal: 'gold', spotMarkup: 0.01 },
+  '24K': { metal: 'gold', spotMarkup: 0.01 },
+  S925: { metal: 'silver', spotMarkup: 0.1, correction: 1 / 0.999 },
+  P950: { metal: 'platinum', spotMarkup: 0.1, correction: 1 / 0.999 },
 };
 
 function createMetalsBaseCostMap(workMarkupMap, purityMap) {
@@ -30,16 +31,18 @@ function createMetalsBaseCostMap(workMarkupMap, purityMap) {
 
 export const METALS_BASE_COST_MAP_CHAIN = createMetalsBaseCostMap(
   {
-    "10K": 5,
-    "14K": 5,
-    "18K": 5,
+    '10K': 5,
+    '14K': 5,
+    '18K': 5,
+    '24K': 5,
     S925: 6,
     P950: 12,
   },
   {
-    "10K": 0.417,
-    "14K": 0.585,
-    "18K": 0.75,
+    '10K': 0.417,
+    '14K': 0.585,
+    '18K': 0.75,
+    '24K': 1,
     S925: 0.925,
     P950: 0.95,
   },
@@ -47,16 +50,18 @@ export const METALS_BASE_COST_MAP_CHAIN = createMetalsBaseCostMap(
 
 export const METALS_BASE_COST_MAP_DIAMOND = createMetalsBaseCostMap(
   {
-    "10K": 10,
-    "14K": 10,
-    "18K": 10,
+    '10K': 10,
+    '14K': 10,
+    '18K': 10,
+    '24K': 10,
     S925: 8,
     P950: 8,
   },
   {
-    "10K": 10 / 24,
-    "14K": 14 / 24,
-    "18K": 18 / 24,
+    '10K': 10 / 24,
+    '14K': 14 / 24,
+    '18K': 18 / 24,
+    '24K': 24 / 24,
     S925: 0.925,
     P950: 0.95,
   },
